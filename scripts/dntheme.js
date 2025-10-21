a67f135b0c2d5a9572e1557b17ce786284d576bb
IK-akx <kustaev.iskander12@gmail>
2025-10-21 15:12:07 +0500

    Sound effects

--- scripts/dntheme.js
+++ scripts/dntheme.js
@@ -1,9 +1,9 @@
-// ======== Theme Toggle (Day/Night) ========
+// Theme Toggle (Day/Night)
 const themeToggleBtn = document.getElementById('themeToggleBtn');
+const clickSound = new Audio('../sounds/click1.wav'); 
 
 // Check theme in localStorage
 let savedTheme = localStorage.getItem('theme');
-
 
 if (savedTheme === 'night') {
   document.body.classList.add('night-theme');
@@ -18,19 +18,22 @@ if (savedTheme === 'night') {
 // Switching theme
 if (themeToggleBtn) {
   themeToggleBtn.addEventListener('click', () => {
+    // Play sound
+    clickSound.currentTime = 0; // reset
+    clickSound.play();
+
     if (document.body.classList.contains('day-theme')) {
       // night
       document.body.classList.add('night-theme');
       document.body.classList.remove('day-theme');
       themeToggleBtn.textContent = "☀️ Day Mode";
-      localStorage.setItem('theme', 'night'); // save
+      localStorage.setItem('theme', 'night');
     } else {
       // day
       document.body.classList.add('day-theme');
       document.body.classList.remove('night-theme');
       themeToggleBtn.textContent = "🌙 Night Mode";
-      localStorage.setItem('theme', 'day'); // save
+      localStorage.setItem('theme', 'day');
     }
   });
 }
-
